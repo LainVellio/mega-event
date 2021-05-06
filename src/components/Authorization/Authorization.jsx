1670372b03ec62a1e44aa3551266dbe2264dfcb8
prybkin <prybkin@nvidia.com>
2021-05-06 20:24:15 +0300

    Autorization refactoring: step 2

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -4,136 +4,121 @@ import { Redirect } from 'react-router';
 
 import Input from '../common/forms/Input';
 import Button from '../common/forms/Button';
-import { email, required } from '../common/validators/validators';
+import { email as emailValidator, required } from '../common/validators/validators';
 import { login, setServerErrorMessage } from '../../redux/reducer';
 import commonStyles from '../../App.module.css';
 import styles from './Authorization.module.css';
 
 class Authorization extends React.Component {
-  constructor(props) {
-    super(props);
-    this.wrapper = React.createRef();
-  }
-  state = {
-    inputs: [
-      {
-        id: 0,
-        isValidate: true,
-      },
-      {
-        id: 1,
-        isValidate: true,
-      },
-    ],
-    email: {
-      value: "",
-      isValidated: false,
-    },
-    password: {
-      value: "",
-      isValidated: false,
-    }
-  };
+	state = {
+		inputValueByKey: {
+			email: '',
+			password: ''
+		},
+		inputValidatedByKey: {
+			email: false,
+			password: false
+		}
+	};
 
-  componentDidMount() {
-    const EMAIL = process.env.REACT_APP_EMAIL || '';
-    const PASSWORD = process.env.REACT_APP_PASSWORD || '';
-    this.setState({
-      ...this.state,
-      inputs: this.state.inputs.map((i) =>
-        i.id === 0 ? { ...i, value: EMAIL } : { ...i, value: PASSWORD },
-      ),
-    });
-  }
+	componentDidMount() {
+		const EMAIL = process.env.REACT_APP_EMAIL || '';
+		const PASSWORD = process.env.REACT_APP_PASSWORD || '';
 
-  setValidate = (id, isValidate) => {
-    this.setState({
-      ...this.state,
-      inputs: this.state.inputs.map((i) =>
-        i.id === Number(id) ? { ...i, isValidate } : i,
-      ),
-    });
-  };
+		this.setState({
+			inputValueByKey: {
+				email: EMAIL,
+				password: PASSWORD
+			}
+		});
+	}
 
-  onChange = (e) => {
-    const id = e.currentTarget.id;
-    const value = e.currentTarget.value;
-    this.props.setServerErrorMessage('');
-    this.setState({
-      ...this.state,
-      inputs: this.state.inputs.map((i) =>
-        i.id === Number(id) ? { ...i, value } : i,
-      ),
-    });
-  };
+	setValidate = (id, isValidate) => {
+		const { inputValidatedByKey } = this.state;
 
-  onClick = () => {
-    const email = this.state.inputs[0].value;
-    const password = this.state.inputs[1].value;
-    this.props.login(email, password);
-  };
+		this.setState({
+			inputValidatedByKey: {
+				...inputValidatedByKey,
+				[id]: isValidate
+			}
+		});
+	};
 
-  isDisabledButton = () => {
-    return (
-      !this.state.inputs.every((i) => i.isValidate === true) ||
-      this.props.isServerProgress
-    );
-  };
-  isDisabledInput = () => {
-    return this.props.isServerProgress;
-  };
+	onChange = (e) => {
+		const { id, value } = e.currentTarget;
+		const { inputValueByKey } = this.state;
 
-  render() {
-    return (
-      <div>
-        {this.props.isAuth && <Redirect to="/questionary" />}
-        <h1 className={commonStyles.h1}>Добро пожаловать</h1>
-        <div className={styles.form}>
-          <div className={styles.inputBlock}>
-            <div className={styles.email}>
-              <Input
-                disabled={this.isDisabledInput()}
-                placeholder='E-mail'
-                type='text'
-                validate={[required, email]}
-                onChange={this.onChange}
-                setValidate={this.setValidate}
-                value={this.state.inputs[0].value}
-                />
-            </div>
+		this.props.setServerErrorMessage('');
+		this.setState({
+			inputValueByKey: {
+				...inputValueByKey,
+				[id]: value
+			}
+		});
+	};
 
-            <div>
-              <Input
-                id={1}
-                disabled={this.isDisabledInput()}
-                placeholder="Пароль"
-                type='password'
-                validate={[required]}
-                onChange={this.onChange}
-                setValidate={this.setValidate}
-                value={this.state.inputs[1].value}
-              />
-              <div className={styles.serverError}>
-                {this.props.serverErrorMessage}
-              </div>
-            </div>
-          </div>
-          <Button onClick={this.onClick} disabled={this.isDisabledButton()}>
-            Войти
-          </Button>
-        </div>
-      </div>
-    );
-  }
+	onLoginClick = () => {
+		const { email, password } = this.state.inputValueByKey;
+		this.props.login(email, password);
+	};
+
+	isButtonDisabled = () => {
+		const { inputValidatedByKey } = this.state;
+		return !Object.values(inputValidatedByKey).every((val) => val) || this.props.isServerProgress;
+	};
+
+	isInputsDisabled = () => {
+		return this.props.isServerProgress;
+	};
+
+	render() {
+		const { password, email } = this.state.inputValueByKey;
+
+		return (
+			<div>
+				{this.props.isAuth && <Redirect to="/questionary" />}
+				<h1 className={commonStyles.h1}>Добро пожаловать</h1>
+				<div className={styles.form}>
+					<div className={styles.inputBlock}>
+						<div className={styles.email}>
+							<Input
+								id="email"
+								disabled={this.isInputsDisabled()}
+								placeholder="E-mail"
+								type="text"
+								validate={[ required, emailValidator ]}
+								onChange={this.onChange}
+								setValidate={this.setValidate}
+								value={email}
+							/>
+						</div>
+						<div>
+							<Input
+								id="password"
+								disabled={this.isInputsDisabled()}
+								placeholder="Пароль"
+								type="password"
+								validate={[ required ]}
+								onChange={this.onChange}
+								setValidate={this.setValidate}
+								value={password}
+							/>
+							<div className={styles.serverError}>{this.props.serverErrorMessage}</div>
+						</div>
+					</div>
+					<Button onClick={this.onLoginClick} disabled={this.isButtonDisabled()}>
+						Войти
+					</Button>
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => ({
-  isAuth: state.reducer.isAuth,
-  isServerProgress: state.reducer.isServerProgress,
-  serverErrorMessage: state.reducer.serverErrorMessage,
-  isListComplete: state.reducer.isListComplete,
+	isAuth: state.reducer.isAuth,
+	isServerProgress: state.reducer.isServerProgress,
+	serverErrorMessage: state.reducer.serverErrorMessage,
 });
 
-export default connect(mapStateToProps, { login, setServerErrorMessage })(
-  Authorization,
-);
+export default connect(mapStateToProps, { login, setServerErrorMessage })(Authorization);
